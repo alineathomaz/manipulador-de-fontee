f60baa3fe92d094d96cc850d73733c99216bd22a
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2022-11-18 01:17:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -25,13 +25,16 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x; //código para buscar a coordenada x do pulso esquerdo, na matriz results vinda do modelo posenet, armazenando-a na variável correspondente
+        rightWristX = results[0].pose.rightWrist.x; //código para buscar a coordenada x do pulso direito, na matriz results vinda do modelo posenet, armazenando-a na variável correspondente
+        difference = floor(leftWristX - rightWristX); // código para encontrar a diferença, subtraindo a coordenada x do pulso esquerdo e direito, removendo os decimais dessa diferença calculada usando a função floor() e armazenando na variável correspondente, ou seja, difference
     }
 }
 function draw(){
     background('#969A97');
-    textSize();
-    fill();
-    text(Sofia, 100, 200);
+    textSize(difference); //textSize() deve conter um número para o tamanho da letra, nesse caso, foi solicitado atualizar com a variável que contém a diferença calculada, ou seja, difference
+    fill('#FFE787'); //deve definir uma cor dentro da função fill() passando uma string de nome de cor, código HEX ou RGB
+    text('Sofia', 100, 200); //a string deve ser apresentada entre aspas dentro dos parâmetros de text()
   }
 
 
